a47134f48851971363435a77e4bea85358eb1155
Tanuja31 <tanujakumawat31@gmail.com>
2020-07-14 17:11:53 +0530

    Initial commit

---
+++ app/src/main/java/com/codelab/dialog/CustomDialog.java
@@ -0,0 +1,32 @@
+package com.codelab.dialog;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+public class CustomDialog extends DialogFragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.layout_dialog,container,false);
+        getDialog().setTitle("Sample");
+        Button doneBtn = (Button) view.findViewById(R.id.btn);
+        doneBtn.setOnClickListener(doneAction);
+        return view;
+    }
+
+    View.OnClickListener doneAction = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(getActivity(),"Test",Toast.LENGTH_LONG).show();
+        }
+    };
+
+}
